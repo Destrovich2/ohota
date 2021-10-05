cea2210d2a97ca14799dbe4d6912946c670328a9
Destrovich2 <jttucker550@gmail.com>
2021-10-05 19:09:59 +0300

    lg3

---
+++ bitrix/modules/landing/install/version.php
@@ -0,0 +1,5 @@
+<?
+$arModuleVersion = array(
+	"VERSION" => "21.400.0",
+	"VERSION_DATE" => "2021-06-17 16:20:00"
+);
---
+++ bitrix/modules/sale/install/version.php
@@ -0,0 +1,5 @@
+<?php
+$arModuleVersion = [
+	'VERSION' => '21.400.100',
+	'VERSION_DATE' => '2021-08-10 08:22:26'
+];
